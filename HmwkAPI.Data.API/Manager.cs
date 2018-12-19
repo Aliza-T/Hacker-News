3bac30c490e03be9c59d5877814ea8bdeed4aeb5
Aliza Kushner <alizatkushner@gmail.com>
2018-12-19 15:29:00 -0500

    Initial Commit

---
+++ HmwkAPI.Data.API/Manager.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HmwkAPI.Data.API
+{
+    public class ManagerRepository
+    {
+        public IEnumerable<int> GetTopTwenty()
+        {
+            var web = new WebClient();
+            string json = web.DownloadString("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+            var list = JsonConvert.DeserializeObject<IEnumerable<int>>(json);
+            return list.Take(20);
+        }
+        public IEnumerable<whatever> GetTheStuff(IEnumerable<int> ids)
+        {
+            var web = new WebClient();
+            ids = GetTopTwenty();
+            var whatever = new List<whatever>();
+            int i = 0;
+            foreach (int id in ids)
+            {
+                 string json = web.DownloadString($"https://hacker-news.firebaseio.com/v0/item/{id}.json?print=pretty");
+                var list = JsonConvert.DeserializeObject<whatever>(json);
+                whatever.Add(list);
+                i++;
+            };
+            return whatever;
+        }
+
+    }
+    public class whatever
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public int Score { get; set; }
+        public string By { get; set; }
+    }
+
+}
---
+++ HmwkAPI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using HmwkAPI.Data.API;
+using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Web.Mvc;
+
+namespace HmwkAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var manager = new ManagerRepository();
+            var list = manager.GetTopTwenty();
+            return View(manager.GetTheStuff(list));
+        }
+
+     
+    }
+}
